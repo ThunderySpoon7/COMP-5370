04e5255f251ec0cba380fecb1f37d13a9b1a5aa9
ThunderySpoon7 <100371865+ThunderySpoon7@users.noreply.github.com>
2025-08-31 13:12:55 -0500

    Loop algorithm in main()

--- P1A/main.py
+++ P1A/main.py
@@ -2,19 +2,11 @@ import sys
 
 ZERO_BYTE = 48
 ONE_BYTE = 49
-
-# Read a two character begin map sequence '(<' from the front of the input string.
-def parse_begin(_input_bytes):
-    res = _input_bytes
-    if (_input_bytes.startswith(b'(<')):
-            res = _input_bytes.removeprefix(b'(<')
-            print('begin-map')
-
-            # Check after beginning of map
-            if (not _input_bytes[:1].isalpha()) or (not _input_bytes.startswith(b'(<')):
-                print('ERROR -- Unexpected character found after map start sequence -->\'(<' + _input_bytes[:10], file=sys.stderr)
-                exit(66)
-    return res
+BIN_BYTES = [48, 49]
+LOWER_ALPHA = [i for i in range(0x61, 0x7b)]
+BEGIN_MAP = b'(<'
+END_MAP = b'>)'
+COMMA_BYTE = 44
 
 # Parse and convert base-10 value from two's compliment binary string
 def parse_num(v:bytes):
@@ -41,65 +33,20 @@ def process_val(v:bytes):
 
     # Parse complex string
 
-# Parses key/value pair from input bytes string
-def parse_kv(_input_bytes:bytes):
-    res = _input_bytes
-    if res[:1].isalpha():
-        try:
-            colon_idx = res.index(b':')
-        except ValueError:
-            # FIXME -- Add error message
-            exit(66)
-            
-        key = res[:colon_idx]
-        res = res.removeprefix(res[:(colon_idx+1)])
-
-        if not (key.islower() and key.isalpha()):
-            print('ERROR -- Keys must only contain lowercase ascii letters a-z', file=sys.stderr)
-            exit(66)
-
-        val_end_pos = None
-        if (b',' in res) and (b'>)' in res):
-            val_end_pos = min(res.index(b','), res.index(b'>)'))
-        elif (b',' in res):
-            val_end_pos = res.index(b',')
-        elif (b'>)' in res):
-            val_end_pos = res.index(b'>)')
-        else:
-            # FIXME -- IDK if this is a good error message
-            print('ERROR -- Missing symbol. Expected \'>)\' at end of map.', file=sys.stderr)
-            exit(66)
-
-        val = res[:val_end_pos]
-        res = res.removeprefix(val)
-
-        val = process_val(val)
-
-# Parses a ',' delimiter separating two key/value pairs
-def parse_delim(_input_bytes):
-    res = _input_bytes
-    if (_input_bytes.startswith(b',')):
-            res = _input_bytes.removeprefix(b',')
-
-            if (not _input_bytes[:1].isalpha()) or (not _input_bytes.startswith(b'(<')):
-                print('ERROR -- Unexpected character found after comma separator -->\',' + _input_bytes[:10], file=sys.stderr)
-                exit(66)
-    return res
-
-def parse_key(ib:bytes):
+def parse_key(inp:bytes) -> tuple[bytes, bytes]:
+    # FIXME -- Implement
     try:
-        colon_idx = ib.index(b':')
+        colon_idx = inp.index(b':')
     except ValueError:
 
         # FIXME -- Add error message
         exit(66)
 
+def parse_val(inp:bytes) -> tuple[bytes, bytes]:
+    pass
+    # FIXME -- Implement
+
 def main():
-    # FIXME -- not sure if this is out of scope of the project
-    if len(sys.argv) < 2:
-        print('ERROR -- Filepath argument not set. Usage: python3 main.py filepath', file=sys.stderr)
-        exit(1)
-    
     filepath = sys.argv[1]
 
     try:
@@ -114,37 +61,68 @@ def main():
     input_bytes = input_bytes.strip()
 
     # Check for root map
-    # FIXME -- Ask if root level map is strictly required
-    if input_bytes.startswith(b'(<'):
-        input_bytes = input_bytes.removeprefix(b'(<')
+    if input_bytes.startswith(BEGIN_MAP):
+        input_bytes = input_bytes.removeprefix(BEGIN_MAP)
     else:
         print('ERROR -- No root level map found. Input must start with \'(<\')', file=sys.stderr)
         exit(66)
 
     nesting_depth = 0 # FIXME -- may not need this variable
     current_map = {} # Initialized to root level map
-    parent_map_trace = [current_map]
+    parent_map_stack = []
 
     # Parse loop
     while (len(input_bytes) > 0):
-        pass
-
         # If first char of input is lower alpha
+        if input_bytes[0] in LOWER_ALPHA:
             # Parse key and value
+            next_key, input_bytes = parse_key(input_bytes)
+            if next_key in current_map:
+                print("ERROR -- Duplicate key found in map: " + next_key.decode())
+                exit(66)
+
+            next_val, input_bytes = parse_val(input_bytes)
+            current_map[next_key] = next_val
             # If start-map jump to beginning of loop
-            # Else continue
+            if next_val == BEGIN_MAP:
+                parent_map_stack.append(current_map)
+                current_map = {}
+                continue
+            # Else next_val is num or string. Proceed
+            else:
+                # parse_val() should catch any other invalidly formatted values so we should be good here
+                # FIXME -- I don't think anything needs to be done here, but I might be wrong
+                pass
         # Else if input starts with >)
-            # Parse end-map
+        elif input_bytes.startswith(END_MAP):
+            input_bytes = input_bytes.removeprefix(END_MAP)
+            current_map = parent_map_stack.pop()
         # Else ERROR
+        else:
+            print("ERROR -- Expected 'key:value' or '>)' after '(<'", file=sys.stderr)
+            exit(66)
 
         #### Probably need to check if we're done with the input
 
         # If first char is comma
+        if input_bytes[0] == COMMA_BYTE:
             # Parse comma
+            input_bytes = input_bytes.removeprefix(b',')
+
             # If next char is alpha jump to beginning of loop
+            if input_bytes[0] in LOWER_ALPHA:
+                continue
             # Else ERROR
+            else:
+                print("ERROR -- Expected key name in lowercase alpha after ','", file=sys.stderr)
+                exit(66)
         # Else if starts with >) jump to beginning of loop
+        elif input_bytes.startswith(END_MAP):
+            continue
         # Else ERROR
+        else:
+            print("ERROR -- Expected ',' or '>)' after 'key:value'", file=sys.stderr)
+            exit(66)
 
     if (nesting_depth != 0):
         print('ERROR -- Missing symbol. Expected \'>)\' at end of map.', file=sys.stderr)
