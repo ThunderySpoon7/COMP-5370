56cf9ab4bed95bba7e0cd002e8afe8832a5f1f50
ThunderySpoon7 <100371865+ThunderySpoon7@users.noreply.github.com>
2025-08-31 20:42:52 -0500

    Replace bytes usage with str

--- P1A/main.py
+++ P1A/main.py
@@ -1,24 +1,21 @@
 import sys, re
 from urllib.parse import unquote
 
-ZERO_BYTE = 48
-ONE_BYTE = 49
-BIN_BYTES = [48, 49]
-LOWER_ALPHA = [i for i in range(0x61, 0x7b)]
-BEGIN_MAP = b'(<'
-END_MAP = b'>)'
-COMMA_BYTE = 44
+LOWER_ALPHA = [chr(i) for i in range(0x61, 0x7b)]
+BEGIN_MAP = "(<"
+END_MAP = ">)"
+COMMA_BYTE = ","
 
-def validate_key(key:bytes):
+def validate_key(key:str):
     if not (key.islower() and key.isalpha()):
-        print(f"ERROR -- Keys must be lowercase alpha strings. Found key: '{key.decode()}'", file=sys.stderr)
+        print(f"ERROR -- Keys must be lowercase alpha strings. Found key: '{key}'", file=sys.stderr)
         exit(66)
 
 # Parses and removes key and ':' from inp
 # returns key, modified inp
-def parse_key(inp:bytes) -> tuple[bytes, bytes]:
+def parse_key(inp:str) -> tuple[str, str]:
     try:
-        key_len = inp.index(b':')
+        key_len = inp.index(':')
     except ValueError:
         print("ERROR -- Expected ':' separating key and value", file=sys.stderr)
         exit(66)
@@ -26,43 +23,43 @@ def parse_key(inp:bytes) -> tuple[bytes, bytes]:
     key = inp[:key_len]
     validate_key(key)
 
-    print(f"{key.decode()} -- ", end="")
+    print(f"{key} -- ", end="")
     return key, inp[key_len+1:]
 
-def decode_num(value:bytes) -> bytes:
+def decode_num(value:str) -> str:
     try:
-        return str(int(value.decode(), 2) - (1 << len(value) if value[0] == ONE_BYTE else 0)).encode()
+        return str(int(value, 2) - (1 << len(value) if value[0] == "1" else 0))
     except ValueError:
         print('ERROR -- Type num value must be represented in twos compliment binary', file=sys.stderr)
         exit(66)
 
-def decode_simple(value:bytes) -> bytes:
+def decode_simple(value:str) -> str:
     return value[:-1]
 
-def decode_complex(value:bytes) -> bytes:
-    return unquote(value).encode()
+def decode_complex(value:str) -> str:
+    return unquote(value)
 
-def parse_val(inp:bytes) -> tuple[bytes, bytes]:
+def parse_val(inp:str) -> tuple[str, str]:
     if inp.startswith(BEGIN_MAP):
         print("map -- ")
         print("begin-map")
         return BEGIN_MAP, inp.removeprefix(BEGIN_MAP)
     try:
-        value_len = re.search(rb'(,|>\))', inp).start()
+        value_len = re.search(r'(,|>\))', inp).start()
     except AttributeError:
         print("ERROR -- Expected ',' or '>)' after 'key:value'", file=sys.stderr)
         exit(66)
 
     value = inp[:value_len]
-    if re.fullmatch(rb'(0|1)*', value):
+    if re.fullmatch(r'(0|1)*', value):
         value = decode_num(value)
-        print(f"num -- {value.decode()}")
-    elif re.fullmatch(rb'([a-zA-Z0-9 \t])*s', value):
+        print(f"num -- {value}")
+    elif re.fullmatch(r'([a-zA-Z0-9 \t])*s', value):
         value = decode_simple(value)
-        print(f"string -- {value.decode()}")
-    elif re.fullmatch(rb'([a-zA-Z]|%[0-9A-F]{2})*%[0-9A-F]{2}([a-zA-Z]|%[0-9A-F]{2})*', value):
+        print(f"string -- {value}")
+    elif re.fullmatch(r'([a-zA-Z]|%[0-9A-F]{2})*%[0-9A-F]{2}([a-zA-Z]|%[0-9A-F]{2})*', value):
         value = decode_complex(value)
-        print(f"string -- {value.decode()}")
+        print(f"string -- {value}")
     else:
         print(f"ERROR -- Invalid data type encoding: '{value}'", file=sys.stderr)
         exit(66)
@@ -71,90 +68,76 @@ def parse_val(inp:bytes) -> tuple[bytes, bytes]:
 
 def main():
     filepath = sys.argv[1]
-
-    try:
-        with open(filepath, 'rb') as input_file:
-            input_bytes = input_file.read()
-    except FileNotFoundError:
-        # FIXME -- Apparently not supposed to do this. Ask how/if we should handle FileNotFound
-        print('ERROR -- File not found. Check FILE argument.', file=sys.stderr)
-        exit(1)
+    with open(filepath, 'r') as input_file:
+        input_str = input_file.read()
 
     # Strip leading and trailing whitespace
-    input_bytes = input_bytes.strip()
+    input_str = input_str.strip()
 
     # Check for root map
-    if input_bytes.startswith(BEGIN_MAP):
-        input_bytes = input_bytes.removeprefix(BEGIN_MAP)
+    if input_str.startswith(BEGIN_MAP):
+        input_str = input_str.removeprefix(BEGIN_MAP)
         print("begin-map")
     else:
         print('ERROR -- No root level map found. Input must start with \'(<\')', file=sys.stderr)
         exit(66)
 
-    nesting_depth = 0 # FIXME -- may not need this variable
     current_map = {} # Initialized to root level map
     parent_map_stack = []
 
-    # Parse loop
-    while (len(input_bytes) > 0):
+    while (len(input_str) > 0):
         # If first char of input is lower alpha
-        if input_bytes[0] in LOWER_ALPHA:
+        if input_str[0] in LOWER_ALPHA:
             # Parse key and value
-            next_key, input_bytes = parse_key(input_bytes)
+            next_key, input_str = parse_key(input_str)
             if next_key in current_map:
-                print("ERROR -- Duplicate key found in map: " + next_key.decode())
+                print("ERROR -- Duplicate key found in map: " + next_key)
                 exit(66)
 
-            next_val, input_bytes = parse_val(input_bytes)
+            next_val, input_str = parse_val(input_str)
             current_map[next_key] = next_val
             # If start-map jump to beginning of loop
             if next_val == BEGIN_MAP:
                 parent_map_stack.append(current_map)
                 current_map = {}
                 continue
-            # Else next_val is num or string. Proceed
-            else:
-                # parse_val() should catch any other invalidly formatted values so we should be good here
-                # FIXME -- I don't think anything needs to be done here, but I might be wrong
-                pass
         # Else if input starts with >)
-        elif input_bytes.startswith(END_MAP):
+        elif input_str.startswith(END_MAP):
             print("end-map")
-            input_bytes = input_bytes.removeprefix(END_MAP)
+            input_str = input_str.removeprefix(END_MAP)
             if len(parent_map_stack) == 0:
                 break
             else:
                 current_map = parent_map_stack.pop()
-
-        # Else ERROR
         else:
             print("ERROR -- Expected 'key:value' or '>)' after '(<'", file=sys.stderr)
             exit(66)
 
-        #### Probably need to check if we're done with the input
-
         # If first char is comma
-        if input_bytes[0] == COMMA_BYTE:
+        if input_str[0] == COMMA_BYTE:
             # Parse comma
-            input_bytes = input_bytes.removeprefix(b',')
+            input_str = input_str.removeprefix(',')
 
             # If next char is alpha jump to beginning of loop
-            if input_bytes[0] in LOWER_ALPHA:
+            if input_str[0] in LOWER_ALPHA:
                 continue
             # Else ERROR
             else:
                 print("ERROR -- Expected key name in lowercase alpha after ','", file=sys.stderr)
                 exit(66)
         # Else if starts with >) jump to beginning of loop
-        elif input_bytes.startswith(END_MAP):
+        elif input_str.startswith(END_MAP):
             continue
         # Else ERROR
         else: # FIXME -- This error message may be redundant with parse_val()
             print("ERROR -- Expected ',' or '>)' after 'key:value'", file=sys.stderr)
             exit(66)
 
-    if len(input_bytes) > 0:
+    if len(input_str) > 0:
         print("ERROR -- Unexpected symbols found after end of root level map.", file=sys.stderr)
+        exit(66)
+    if len(parent_map_stack) > 0:
+        print("ERROR -- At least one '>)' missing at end of input", file=sys.stderr)
         exit(66)
 
 main()
